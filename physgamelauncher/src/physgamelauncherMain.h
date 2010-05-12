a0c1fbbf9815cd3e4dc4a1c9fc5bf836af24f3a7
unknown <hydrowolfy@.(none)>
2010-05-11 22:32:54 -0500

    Made some changes to

--- physgamelauncher/src/physgamelauncherMain.h
+++ physgamelauncher/src/physgamelauncherMain.h
@@ -11,12 +11,14 @@
 #define PHYSGAMELAUNCHERMAIN_H
 
 //(*Headers(physgamelauncherFrame)
-#include <wx/menu.h>
+#include <wx/panel.h>
+#include <wx/button.h>
 #include <wx/frame.h>
-#include <wx/statusbr.h>
-#include "zzip.h"
-
 //*)
+
+using namespace std;
+
+
 
 class physgamelauncherFrame: public wxFrame
 {
@@ -32,18 +34,24 @@ class physgamelauncherFrame: public wxFrame
         void RunGame(wxCommandEvent& event);
         void OnAbout(wxCommandEvent& event);
         void OnButton1Click(wxCommandEvent& event);
+        bool FileExists(string strFilename);
+
         //*)
 
         //(*Identifiers(physgamelauncherFrame)
-        static const long RUN_GAME;
-        static const long idMenuQuit;
-        static const long idMenuAbout;
-        static const long ID_STATUSBAR1;
+        static const long RUN_GAME2;
+        static const long ID_BUTTON2;
+        static const long ID_BUTTON3;
+        static const long idMenuQuit2;
+        static const long ID_PANEL1;
         //*)
 
         //(*Declarations(physgamelauncherFrame)
-        wxMenuItem* MenuItem3;
-        wxStatusBar* StatusBar1;
+        wxButton* Button4;
+        wxButton* Button1;
+        wxPanel* Panel1;
+        wxButton* Button2;
+        wxButton* Button3;
         //*)
 
         DECLARE_EVENT_TABLE()
