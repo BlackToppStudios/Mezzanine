bcd0c5ca354222ae4c9ed558b20f9cab19cf8063
unknown <Mako@.(none)>
2010-06-08 12:42:23 -0500

    Fixed weird syntax error in my previous commit.

--- physgameengine/src/quaternion.cpp
+++ physgameengine/src/quaternion.cpp
@@ -1,4 +1,4 @@
-﻿//© Copyright 2010 Joseph Toppi and John Blackwood
+//� Copyright 2010 Joseph Toppi and John Blackwood
 /* This file is part of The PhysGame Engine.
 
     The PhysGame Engine is free software: you can redistribute it and/or modify
