d719d45a00111bb2e7e6f7df4b8c120afccfc52d
unknown <Mako@.(none)>
2010-10-10 14:32:41 -0600

    minor fixes for pulling a suck on my part

--- Catch!/src/itemshoplist.cpp
+++ Catch!/src/itemshoplist.cpp
@@ -8,8 +8,8 @@ ItemShopList::ItemShopList(Real X, Real Y, Real Width, Real Height, UILayer* Lay
     GraphMan = TheWorld->GetGraphicsManager();
     UIMan = TheWorld->GetUIManager();
     ParentLayer = Layer;
-    Whole WWidth = UIMan->getRenderWidth();
-    Whole WHeight = UIMan->getRenderHeight();
+    Whole WWidth = GraphMan->getRenderWidth();
+    Whole WHeight = GraphMan->getRenderHeight();
     Backdrop = Layer->CreateRectangle(WWidth*X,WHeight*Y,WWidth*Width,WHeight*Height);
     ListArea = NULL;
     SideScroll = NULL;
@@ -36,15 +36,15 @@ void ItemShopList::SetDistanceBetweenButtons(Real Distance)
 
 void ItemShopList::CreateListArea(Real X, Real Y, Real Width, Real Height)
 {
-    Whole WWidth = UIMan->getRenderWidth();
-    Whole WHeight = UIMan->getRenderHeight();
+    Whole WWidth = GraphMan->getRenderWidth();
+    Whole WHeight = GraphMan->getRenderHeight();
     ListArea = ParentLayer->CreateRectangle(WWidth*X,WHeight*Y,WWidth*Width,WHeight*Height);
     Vector2 LAP = ListArea->GetPosition();
     Vector2 LAS = ListArea->GetSize();
-    SideScrollBack = ParentLayer->CreateRectangle(LAP.X * 0.94, LAP.Y * 0.4, LAS.X * 0.05, LAS.Y * 0.95);
+    SideScrollBack = ParentLayer->CreateRectangle((LAS.X * 0.94)+LAP.X, (LAS.Y * 0.04)+LAP.Y, LAS.X * 0.05, LAS.Y * 0.94);
     Vector2 SSBP = SideScrollBack->GetPosition();
     Vector2 SSBS = SideScrollBack->GetSize();
-    SideScroll = ParentLayer->CreateRectangle(SSBP * 0.005, SSBP * 0.005, SSBS.X * 0.95, SSBS.Y * 0.95);
+    SideScroll = ParentLayer->CreateRectangle((SSBP.X * 0.005), (SSBP.Y * 0.005), SSBS.X * 0.95, SSBS.Y * 0.95);
 }
 
 void ItemShopList::AddItemToList(UIButton* Item)
