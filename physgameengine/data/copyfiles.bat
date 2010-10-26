15d4f908115ce883a11be75e10d8c509888f6157
Sqeaky <toppij@gmail.com>
2010-10-26 13:34:45 -0500

    I began reworking the file copy part of both the windows and linuxbuild process. This will make using a variety of IDE and build system easier.

---
+++ physgameengine/data/copyfiles.bat
@@ -0,0 +1,41 @@
+rem © Copyright 2010 BlackTopp Studios Inc.
+rem This file is part of The PhysGame Engine and is used during the build process to copy files.
+rem
+rem    The PhysGame Engine is free software: you can redistribute it and/or modify
+rem    it under the terms of the GNU General Public License as published by
+rem    the Free Software Foundation, either version 3 of the License, or
+rem    (at your option) any later version.
+rem
+rem    The PhysGame Engine is distributed in the hope that it will be useful,
+rem    but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem    GNU General Public License for more details.
+rem
+rem    You should have received a copy of the GNU General Public License
+rem    along with The PhysGame Engine.  If not, see <http://www.gnu.org/licenses/>.
+rem
+rem The original authors have included a copy of the license specified above in the
+rem   'Docs' folder. See 'gpl.txt'
+rem
+rem We welcome the use of The PhysGame anyone, including companies who wish to
+rem   Build professional software and charge for their product.
+rem
+rem   However there are some practical restrictions, so if your project involves
+rem   any of the following you should contact us and we will try to work something
+rem   out:
+rem    - DRM or Copy Protection of any kind(except Copyrights)
+rem    - Software Patents You Do Not Wish to Freely License
+rem    - Any Kind of Linking to Non-GPL licensed Works
+rem    - Are Currently In Violation of Another Copyright Holder's GPL License
+rem    - If You want to change our code and not add a few hundred MB of stuff to
+rem        your distribution
+rem
+rem   These and other limitations could cause serious legal problems if you ignore
+rem   them, so it is best to simply contact us or the Free Software Foundation, if
+rem   you have any questions.
+rem
+rem   Joseph Toppi - toppij@gmail.com
+rem   John Blackwood - makoenergy02@gmail.com
+
+
+
