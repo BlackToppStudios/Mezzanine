4ac96cb0efbcb9f8a1ae2ef34be1c11dfd8a5615
Sqeaky <toppij@gmail.com>
2010-10-27 13:21:56 -0500

    Started work on a c++ version of the build system because use BAT files seems impractical. This should be pretty easy, as all the logic is worked out already and really we could use this on other systems if it is carefully coded.

---
+++ tools/CopyFilesTool/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define WINDOWS
+#define GNU
+
+void CopyFile(string Source, string Destination);
+
+int main(int ArgCount, char** ArgValueArray)
+{
+    if(2 == ArgCount)
+    {
+        cout<< "Do Logic Here";
+    }else{
+        cout<< "Display usage here";
+    }
+
+    return 0;
+}
+
+
+void CopyFile(string Source, string Destination)
+{
+    #ifdef WINDOWS
+    //system()
+    #endif
+
+    #ifdef GNU
+
+    #endif
+}
