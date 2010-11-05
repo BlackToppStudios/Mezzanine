515afca024d6d55c1160cbe029b650a2564df731
Sqeaky on windirs <toppij@gmail.com>
2010-11-04 22:50:55 -0500

    The windows build system now builds bullet if it is missing. Missing is now the default state for bullet because having an incorrect bullet library will cause linking problems

--- physgameengine/data/windows/copyspecificfiles.bat
+++ physgameengine/data/windows/copyspecificfiles.bat
@@ -1,3 +1,19 @@
+@echo off
+rem Check if bullet exists
+if exist data\windows\bullet\libBullet.a goto EndBulletExistIf 
+	cd libincludes\common\bulletsrc
+	if exist "c:\Program Files (x86)\" goto BuildBullet64
+		:BuildBullet32
+		"c:\Program Files (x86)\CodeBlocks\codeblocks.exe" --build --target=Release BulletWorkspace.workspace
+		goto EndBulletBuildIf
+		:BuildBullet64
+		"c:\Program Files (x86)\CodeBlocks\codeblocks.exe" --build --target=Release BulletWorkspace.workspace
+		goto EndBulletBuildIf
+	:EndBulletBuildIf
+	copy libBullet.a ..\..\..\data\windows\bullet\
+	cd ..\..\..\
+:EndBulletExistIf
+
 cmd /c if not exist ".\%1\data\windows\ogre" mkdir .\%1\data\windows\ogre
 cmd /c copy /y /v .\data\windows\ogre\OgreMain.dll .\%1
 cmd /c copy /y /v .\data\windows\sdl\SDL.dll .\%1
