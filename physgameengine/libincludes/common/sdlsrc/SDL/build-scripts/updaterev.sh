6baa3eb95629c41395a876443a79305b95d40ca0
Sqeaky <toppij@gmail.com>
2011-02-18 12:26:39 -0600

    I am adding sdl from the latest available source release to the library

---
+++ physgameengine/libincludes/common/sdlsrc/SDL/build-scripts/updaterev.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Generate a header file with the current source revision
+
+outdir=`pwd`
+cd `dirname $0`
+srcdir=..
+header=$outdir/include/SDL_revision.h
+
+rev=`sh showrev.sh`
+if [ "$rev" != "" -a "$rev" != "hg-0:baadf00d" ]; then
+    echo "#define SDL_REVISION \"$rev\"" >$header.new
+    if diff $header $header.new >/dev/null 2>&1; then
+        rm $header.new
+    else
+        mv $header.new $header
+    fi
+fi
