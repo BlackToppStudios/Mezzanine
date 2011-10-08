81e4391abbfe10e22deca7a007cd01563885e026
Sqeaky <toppij@blacktoppstudios.com>
2011-10-08 13:53:10 -0500

    I updated SDL from the mercurial repo

--- physgameengine/libincludes/common/sdlsrc/SDL/include/SDL_opengl.h
+++ physgameengine/libincludes/common/sdlsrc/SDL/include/SDL_opengl.h
@@ -37,6 +37,11 @@
 #endif
 #include <windows.h>
 #endif
+
+#ifdef __HAIKU__  /* !!! FIXME: temp compiler warning fix... */
+#define NO_SDL_GLEXT 1
+#endif
+
 #ifdef __glext_h_
 /* Someone has already included glext.h */
 #define NO_SDL_GLEXT
@@ -5105,20 +5110,34 @@ typedef char GLchar;
 
 #ifndef GL_VERSION_1_5
 /* GL types for handling large vertex buffer objects */
+#if defined(__APPLE__)
+typedef long GLintptr;
+typedef long GLsizeiptr;
+#else
 typedef ptrdiff_t GLintptr;
 typedef ptrdiff_t GLsizeiptr;
+#endif
 #endif
 
 #ifndef GL_ARB_vertex_buffer_object
 /* GL types for handling large vertex buffer objects */
+#if defined(__APPLE__)
+typedef long GLintptrARB;
+typedef long GLsizeiptrARB;
+#else
 typedef ptrdiff_t GLintptrARB;
 typedef ptrdiff_t GLsizeiptrARB;
+#endif
 #endif
 
 #ifndef GL_ARB_shader_objects
 /* GL types for program/shader text and shader object handles */
 typedef char GLcharARB;
+#if defined(__APPLE__)
+typedef void *GLhandleARB;
+#else
 typedef unsigned int GLhandleARB;
+#endif
 #endif
 
 /* GL type for "half" precision (s10e5) float data in host memory */
--- physgameengine/libincludes/common/sdlsrc/SDL/src/main/beos/SDL_BeApp.cc
+++ physgameengine/libincludes/common/sdlsrc/SDL/src/main/beos/SDL_BeApp.cc
@@ -27,11 +27,17 @@
 #include <storage/Entry.h>
 #include <unistd.h>
 
+#include "SDL_BApp.h"	/* SDL_BApp class definition */
 #include "SDL_BeApp.h"
 #include "SDL_thread.h"
 #include "SDL_timer.h"
 #include "SDL_error.h"
 
+#include "../../video/bwindow/SDL_BWin.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 /* Flag to tell whether or not the Be application is active or not */
 int SDL_BeAppActive = 0;
 static SDL_Thread *SDL_AppThread = NULL;
@@ -41,7 +47,7 @@ StartBeApp(void *unused)
 {
     BApplication *App;
 
-    App = new BApplication("application/x-SDL-executable");
+    App = new SDL_BApp("application/x-SDL-executable");
 
     App->Run();
     delete App;
@@ -54,7 +60,7 @@ SDL_InitBeApp(void)
 {
     /* Create the BApplication that handles appserver interaction */
     if (SDL_BeAppActive <= 0) {
-        SDL_AppThread = SDL_CreateThread(StartBeApp, NULL);
+        SDL_AppThread = SDL_CreateThread(StartBeApp, "SDLApplication", NULL);
         if (SDL_AppThread == NULL) {
             SDL_SetError("Couldn't create BApplication thread");
             return (-1);
@@ -110,4 +116,18 @@ SDL_QuitBeApp(void)
     }
 }
 
+
 /* vi: set ts=4 sw=4 expandtab: */
+#ifdef __cplusplus
+}
+#endif
+
+/* SDL_BApp functions */
+void SDL_BApp::ClearID(SDL_BWin *bwin) {
+	_SetSDLWindow(NULL, bwin->GetID());
+	int32 i = _GetNumWindowSlots() - 1;
+	while(i >= 0 && GetSDLWindow(i) == NULL) {
+		_PopBackWindow();
+		--i;
+	}
+}
