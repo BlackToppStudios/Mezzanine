b22f32bd99a54562fbb1294b6c1997827384d503
Sqeaky <toppij@blacktoppstudios.com>
2013-02-25 06:14:43 -0600

    Emitting the enum is now complete, but the class definitions only look complete. Just just need their data plugged in and then some testing.

--- docs/ToolsExceptions/CppExceptionCreator.lua
+++ docs/ToolsExceptions/CppExceptionCreator.lua
@@ -7,6 +7,11 @@ function trim(s)
   return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
+function GetEnumRemark(Class, Dox)
+  return ( "   ///< When used a instance of " .. Class .. " is thrown. " .. Dox .. "\n" )
+end
+
+
 SectionSeparator = "///////////////////////////////////////////////////////////////////////////////\n"
 
 EnumOutput = SectionSeparator
@@ -14,48 +19,78 @@ EnumOutput = EnumOutput .. "// Exception code values\n"
 EnumOutput = EnumOutput .. "enum ExceptionCodes\n{\n"
 
 DefinitionOutput = SectionSeparator
+DefinitionOutput = DefinitionOutput .. "// Exception code class definitions.\n\n"
 
 First = false
+EnumIdentifier = ""
+EnumValue = ""
+ClassName = ""
+InheritsFrom = ""
+ClassBrief = ""
 
 Input = io.open("ExceptionDefinition.txt","r")
 for line in Input:lines() do
 	if string.sub(line,1,1) == "#" then
 		-- Ignore remarked lines
-	else
-		
+	else	
 		if First == false then
 			First = true			
 		else
-			EnumOutput = EnumOutput .. ", ///<\n"
+			EnumOutput = EnumOutput .. ", " .. GetEnumRemark(ClassName, ClassBrief)
 		end
 
 		EnumIdentifier = trim(line:sub(1,39))
 		EnumValue = trim(line:sub(40,49))
 		ClassName = trim(line:sub(53,94))
-		InheritsFrom = trim(line:sub(95))
+		InheritsFrom = trim(line:sub(95,106))
+		ClassBrief = trim(line:sub(129))
+		print (EnumIdentifier .. ", " .. EnumValue .. ", " .. ClassName .. ", " .. InheritsFrom .. ", " .. ClassBrief)
 
-		CurrentLine = EnumIdentifier
-
-		-- print (EnumIdentifier .. ", " .. EnumValue .. ", " .. ClassName .. ", " .. InheritsFrom .. ", ")
-		
-
-		SpaceCount = 40 - CurrentLine:len()
-		print (SpaceCount)
+		CurrentEnumLine = "    " .. EnumIdentifier
+		SpaceCount = 40 - CurrentEnumLine:len()
 		for i = 1,SpaceCount, 1 do
-		--for i = 1,50, 1 do
-			--print (i)
-			CurrentLine = CurrentLine .. " "
+			CurrentEnumLine = CurrentEnumLine .. " "
 		end
-		CurrentLine = CurrentLine .. " = " .. CurrentLine
-		--os.exit
+		CurrentEnumLine = CurrentEnumLine .. " = " .. EnumValue
+		EnumOutput = EnumOutput .. CurrentEnumLine
 
-		EnumOutput = EnumOutput .. CurrentLine
+		DefinitionOutput = DefinitionOutput .. SectionSeparator
+                DefinitionOutput = DefinitionOutput  .. "/// @brief " .. ClassBrief .. "\n"
+                DefinitionOutput = DefinitionOutput  .. "///////////////////\n"
+                DefinitionOutput = DefinitionOutput  .. "class MEZZ_LIB NotImplementedException : public Exception\n"
+                DefinitionOutput = DefinitionOutput  .. "{\n"
+                DefinitionOutput = DefinitionOutput  .. "public:\n"
+                DefinitionOutput = DefinitionOutput  .. "    /// @brief The internal code for this exception.\n"
+                DefinitionOutput = DefinitionOutput  .. "    static const Whole ExceptionCode = Exception::NOT_IMPLEMENTED_EXCEPTION;\n"
+                DefinitionOutput = DefinitionOutput  .. "\n"
+                DefinitionOutput = DefinitionOutput  .. "    /// @brief Class constructor.\n"
+                DefinitionOutput = DefinitionOutput  .. "    /// @param Message A basic description of the error.\n"
+                DefinitionOutput = DefinitionOutput  .. "    /// @param SrcFunction The name of the function from which this originated.\n"
+                DefinitionOutput = DefinitionOutput  .. "    /// @param SrcFile The name of the file from which this originated.\n"
+                DefinitionOutput = DefinitionOutput  .. "    /// @param FileLine The line on the named file from which this originated.\n"
+                DefinitionOutput = DefinitionOutput  .. "    NotImplementedException(const String& Message, const String& SrcFunction, const String& SrcFile, const Whole& FileLine)\n"
+                DefinitionOutput = DefinitionOutput  .. "        : Exception(\"NotImplementedException\",Message,SrcFunction,SrcFile,FileLine)\n"
+                DefinitionOutput = DefinitionOutput  .. "        {}\n"
+                DefinitionOutput = DefinitionOutput  .. "    /// @brief Class destructor.\n"
+                DefinitionOutput = DefinitionOutput  .. "    virtual ~NotImplementedException() throw() {}\n"
+                DefinitionOutput = DefinitionOutput  .. "\n"
+                DefinitionOutput = DefinitionOutput  .. "    /// @copydoc Exception::GetExceptionCode()\n"
+                DefinitionOutput = DefinitionOutput  .. "    virtual Whole GetExceptionCode() const throw()\n"
+                DefinitionOutput = DefinitionOutput  .. "        { return NotImplementedException::ExceptionCode; }\n"
+                DefinitionOutput = DefinitionOutput  .. "};//NotImplementedException\n"
+                DefinitionOutput = DefinitionOutput  .. "\n"
+                DefinitionOutput = DefinitionOutput  .. "template<>\n"
+                DefinitionOutput = DefinitionOutput  .. "struct MEZZ_LIB ExceptionFactory<NotImplementedException::ExceptionCode>\n"
+                DefinitionOutput = DefinitionOutput  .. "{ typedef NotImplementedException Type; };\n"
+                DefinitionOutput = DefinitionOutput  .. "\n"
+                DefinitionOutput = DefinitionOutput  .. "\n"
 	end
 
 end
-
-EnumOutput = EnumOutput .. "  ///<\n"
 Input:close()
+
+EnumOutput = EnumOutput .. "  " .. GetEnumRemark(ClassName, ClassBrief) .. "};"
+
 
 
 print(EnumOutput)
