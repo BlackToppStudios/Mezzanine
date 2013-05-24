95ef452232dd2e5cc8b315ed5cc66438604cc901
MakoEnergy <makoenergy02@gmail.com>
2013-05-24 14:19:28 -0500

    Completed the initial implementation of the wav decoder.

--- Mezzanine/src/Audio/wavdecoder.cpp
+++ Mezzanine/src/Audio/wavdecoder.cpp
@@ -40,22 +40,240 @@
 #ifndef _audiowavdecoder_cpp
 #define _audiowavdecoder_cpp
 
+#ifdef ENABLE_WAV_ENCODE
+
 #include "Audio/wavdecoder.h"
+
+#include <cstring>
 
 namespace Mezzanine
 {
     namespace Audio
     {
-        WavDecoder::WavDecoder()
+        WavDecoder::WavDecoder(Resource::DataStreamPtr Stream)
+            : WavStream(Stream),
+              Valid(false),
+              Channels(0),
+              BlockAlign(0),
+              BitsPerSample(0),
+              SampleRate(0),
+              ByteRate(0),
+              DataSize(0),
+              DataOffset(0)
         {
+            const char* RIFFTAG = "RIFF";
+            const char* WAVETAG = "WAVE";
+            const char* FORMATTAG = "fmt ";
+            const char* DATATAG = "data";
 
+            char Ident[4];
+            Int32 Temp32 = 0;
+            Int16 Temp16 = 0;
+            Int8 Temp8 = 0;
+            UInt32 StartOffset = 0;
+
+            //Read the first 4 bytes
+            this->WavStream->SetStreamPosition(0);
+            this->WavStream->Read(Ident,4);
+            //Check to see if it is a valid RIFF file
+            if( strncmp(Ident,RIFFTAG,4) == 0 )
+            {
+                this->WavStream->Read(&Temp32,4);
+                //Check to see if the file is big enough to be valid (not completely accurate)
+                if( Temp32 >= 44 )
+                {
+                    this->WavStream->Read(Ident,4);
+                    //Check that it is a wave file
+                    if( strncmp(Ident,WAVETAG,4) == 0 )
+                    {
+                        //Save our position
+                        StartOffset = this->WavStream->GetStreamPosition();
+                        //Scan for the first fmt chuck (not necessarily right after)
+                        do{
+                            this->WavStream->Read(Ident,4);
+                        } while( ( strncmp(Ident,FORMATTAG,4) != 0 ) && ( this->WavStream->GetStreamPosition() < this->WavStream->GetSize() ) )
+                        //Did we find it?
+                        if( this->WavStream->GetStreamPosition() < ( this->WavStream->GetSize() - 16 ) )
+                        {
+                            //Yes, read it in
+                            this->WavStream->Read(&Temp32,4);
+                            if( Temp32 >= 16 )
+                            {
+                                //Check that it is in PCM format, we don't support compressed wavs
+                                this->WavStream->Read(&Temp16,2);
+                                this->Channels = Temp16;
+                                //We only support mono or stereo wavs
+                                if( this->Channels == 1 || this->Channels == 2 )
+                                {
+                                    this->WavStream->Read(&Temp32,4);
+                                    this->SampleRate = Temp32;
+                                    this->WavStream->Read(&Temp32,4);
+                                    this->ByteRate = Temp32;
+                                    this->WavStream->Read(&Temp16,2);
+                                    this->BlockAlign = Temp16;
+                                    this->WavStream->Read(&Temp16,2);
+                                    this->BitsPerSameple = Temp16;
+
+                                    //We only support 8 bit or 16 bit wavs
+                                    if( this->BitsPerSample == 8 || this->BitsPerSample == 16 )
+                                    {
+                                        //Reset our pointer to start scanning for the data block
+                                        this->WavStream->SetStreamPosition(StartOffset);
+										//Scan for the first data chuck (not necessarily right after)
+                                        do{
+                                            this->WavStream->Read(Ident,4);
+                                        } while( ( strncmp(Ident,DATATAG,4) != 0 ) && ( this->WavStream->GetStreamPosition() < this->WavStream->GetSize() ) )
+
+                                        //Did we find it?
+                                        if( this->WavStream->GetStreamPosition() < this->WavStream->GetSize() )
+                                        {
+                                            //Get size of data block
+                                            this->WavStream->Read(&Temp32,4);
+                                            this->DataSize = Temp32;
+                                            this->DataOffset = this->WavStream->GetStreamPosition();
+                                            this->Valid = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         WavDecoder::~WavDecoder()
         {
+        }
 
+        ///////////////////////////////////////////////////////////////////////////////
+        // Utility
+
+        bool WavDecoder::IsValid()
+        {
+            return this->Valid;
+        }
+
+        Audio::Encoding WavDecoder::GetEncoding() const
+        {
+            return Audio::Enc_WAV;
+        }
+
+        bool WavDecoder::IsSeekingSupported()
+        {
+            return true;
+        }
+
+        Audio::BitConfig WavDecoder::GetBitConfiguration() const
+        {
+            if(this->Channels == 1 && this->BitsPerSample == 8)
+                return Audio::BC_8Bit_Mono;
+            else if(this->Channels == 1 && this->BitsPerSample == 16)
+                return Audio::BC_16Bit_Mono;
+            else if(this->Channels == 2 && this->BitsPerSample == 8)
+                return Audio::BC_8Bit_Stereo;
+            else if(this->Channels == 2 && this->BitsPerSample == 16)
+                return Audio::BC_16Bit_Stereo;
+            else
+                return Audio::BC_8Bit_Mono;//fallback return
+        }
+
+        UInt32 WavDecoder::GetFrequency() const
+        {
+            return this->SampleRate;
+        }
+
+        Resource::DataStreamPtr WavDecoder::GetStream() const
+        {
+            return this->WavStream;
+        }
+
+        bool WavDecoder::SetPosition(Int32 Position, bool Relative)
+        {
+            Int32 CurrPos = this->WavStream->GetStreamPosition();
+            Int32 StartPos = this->DataOffset;
+            Int32 EndPos = this->DataOffset + this->DataSize;
+
+            if( Relative ) {
+                if( CurrPos + Position < StartPos ) Position = StartPos;
+                else if( CurrPos + Position > EndPos ) Position = EndPos;
+            }else{
+                if( Position < StartPos ) Position = StartPos;
+                else if( Position > EndPos ) Position = EndPos;
+            }
+
+            this->WavStream->SetStreamPosition(Position);
+            return true;
+        }
+
+        bool WavDecoder::Seek(const Real Seconds, bool Relative)
+        {
+            Int32 SeekInBytes = ( Seconds * static_cast<Real>(this->SampleRate) * static_cast<Real>(this->Channels) * (static_cast<Real>(this->BitsPerSample) / 8.0) );
+            return this->SetPosition(SeekInBytes,Relative);
+        }
+
+        UInt32 WavDecoder::ReadAudioData(void* Output, UInt32 Amount)
+        {
+            Int32 CurrPos = this->WavStream->GetStreamPosition();
+            Int32 StartPos = this->DataOffset;
+            Int32 EndPos = this->DataOffset + this->DataSize;
+            Int32 ReadClamped = Amount;
+
+            if( CurrPos > EndPos )
+                return 0;
+
+            if( CurrPos < StartPos )
+            {
+                this->WavStream->SetStreamPosition(StartPos);
+                CurrPos = this->WavStream->GetStreamPosition();
+            }
+
+            if( CurrPos + ReadClamped > EndPos )
+                ReadClamped = EndPos - CurrPos;
+
+            if( ReadClamped < 0 )
+                return 0;
+
+            return this->WavStream->Read(Output,ReadClamped);
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////
+        // Stream Stats
+
+        Real WavDecoder::GetTotalTime() const
+        {
+            Real Second = ( static_cast<Real>(this->SampleRate) * static_cast<Real>(this->Channels) * (static_cast<Real>(this->BitsPerSample) / 8.0) )
+            return static_cast<Real>(this->WavStream->GetSize()) / Second;
+        }
+
+        Real WavDecoder::GetCurrentTime() const
+        {
+            Real Second = ( static_cast<Real>(this->SampleRate) * static_cast<Real>(this->Channels) * (static_cast<Real>(this->BitsPerSample) / 8.0) )
+            return static_cast<Real>(this->WavStream->GetStreamPosition()) / Second;
+        }
+
+        UInt32 WavDecoder::GetTotalSize() const
+        {
+            return this->WavStream->GetSize();
+        }
+
+        UInt32 WavDecoder::GetCompressedSize() const
+        {
+            return this->WavStream->GetSize();
+        }
+
+        UInt32 WavDecoder::GetCurrentPosition() const
+        {
+            return this->WavStream->GetStreamPosition();
+        }
+
+        UInt32 WavDecoder::GetCurrentCompressedPosition() const
+        {
+            return this->WavStream->GetStreamPosition();
         }
     }//Audio
 }//Mezzanine
+
+#endif //ENABLE_WAV_ENCODE
 
 #endif
--- Mezzanine/src/Audio/wavdecoder.h
+++ Mezzanine/src/Audio/wavdecoder.h
@@ -40,22 +40,98 @@
 #ifndef _audiowavdecoder_h
 #define _audiowavdecoder_h
 
+#ifdef ENABLE_WAV_ENCODE
+
 #include "Audio/decoder.h"
 
 namespace Mezzanine
 {
     namespace Audio
     {
-        class MEZZ_LIB WavDecoder
+        ///////////////////////////////////////////////////////////////////////////////
+        /// @brief This is a @ref iDecoder implementation for the wav encoding.
+        /// @details
+        ///////////////////////////////////////
+        class MEZZ_LIB WavDecoder : public iDecoder
         {
         protected:
+            /// @internal
+            /// @brief This is a shared pointer to the stream being decoded.
+            Resource::DataStreamPtr WavStream;
+            /// @internal
+            /// @brief Tracks and stores the validity of the Wav stream.
+            bool Valid;
+            /// @internal
+            /// @brief The number of channels in this wav file.
+            UInt16 Channels;
+            /// @internal
+            /// @brief The byte alignment of the waveform data in this wav file.
+            UInt16 BlockAlign;
+            /// @internal
+            /// @brief The size of each sample in this wav file.
+            UInt16 BitsPerSample;
+            /// @internal
+            /// @brief The frequency of the wav file.
+            Int32 SampleRate;
+            /// @internal
+            /// @brief Redundant wav data expressing the rate of platback for the wav file.
+            Int32 ByteRate;
+            /// @internal
+            /// @brief The end point of the audio in the wav file.
+            Int32 DataSize;
+            /// @internal
+            /// @brief The start point of the audio in the wav file.
+            Int32 DataOffset;
         public:
             /// @brief Class constructor.
-            WavDecoder();
+            /// @param Stream The stream to decode.
+            WavDecoder(Resource::DataStreamPtr Stream);
             /// @brief Class destructor.
             virtual ~WavDecoder();
+
+            ///////////////////////////////////////////////////////////////////////////////
+            // Utility
+
+            /// @copydoc iDecoder::IsValid()
+            virtual bool IsValid();
+            /// @copydoc iDecoder::GetEncoding() const
+            virtual Audio::Encoding GetEncoding() const;
+            /// @copydoc iDecoder::IsSeekingSupported()
+            virtual bool IsSeekingSupported();
+            /// @copydoc iDecoder::GetBitConfiguration() const
+            virtual Audio::BitConfig GetBitConfiguration() const;
+            /// @copydoc iDecoder::GetFrequency() const
+            virtual UInt32 GetFrequency() const;
+            /// @copydoc iDecoder::GetStream() const
+            virtual Resource::DataStreamPtr GetStream() const;
+
+            /// @copydoc iDecoder::SetPosition(Int32 Position, bool Relative)
+            virtual bool SetPosition(Int32 Position, bool Relative);
+            /// @copydoc iDecoder::Seek(const Real Seconds, bool Relative)
+            virtual bool Seek(const Real Seconds, bool Relative);
+
+            /// @copydoc iDecoder::ReadAudioData(void* Output, UInt32 Amount)
+            virtual UInt32 ReadAudioData(void* Output, UInt32 Amount);
+
+            ///////////////////////////////////////////////////////////////////////////////
+            // Stream Stats
+
+            /// @copydoc iDecoder::GetTotalTime() const
+            virtual Real GetTotalTime() const;
+            /// @copydoc iDecoder::GetCurrentTime() const
+            virtual Real GetCurrentTime() const;
+            /// @copydoc iDecoder::GetTotalSize() const
+            virtual UInt32 GetTotalSize() const;
+            /// @copydoc iDecoder::GetCompressedSize() const
+            virtual UInt32 GetCompressedSize() const;
+            /// @copydoc iDecoder::GetCurrentPosition() const
+            virtual UInt32 GetCurrentPosition() const;
+            /// @copydoc iDecoder::GetCurrentCompressedPosition() const
+            virtual UInt32 GetCurrentCompressedPosition() const;
         };//WavDecoder
     }//Audio
 }//Mezzanine
+
+#endif //ENABLE_WAV_ENCODE
 
 #endif
