e7f5e12a4e08ef82bcc47e3b863af7cc894072b8
Mako_Energy <makoenergy02@gmail.com>
2013-07-07 02:27:55 -0500

    Documentation ammendment.

--- Mezzanine/src/Audio/OALS/oalsaudiomanager.cpp
+++ Mezzanine/src/Audio/OALS/oalsaudiomanager.cpp
@@ -827,6 +827,7 @@ namespace Mezzanine
 
                     this->TheEntresol->GetScheduler().AddWorkUnit( this->EffectFilterCleanWork );
                     this->EffectFilterCleanWork->AddDependency( this->BufferUpdate2DWork );
+                    /// @todo Some of this logic may need to be moved to the registration method if we want to support dynamic created and destroyed worlds.
                     for( SoundScapeManagerIterator SSM = this->SoundScapeManagers.begin() ; SSM != this->SoundScapeManagers.end() ; ++SSM )
                         this->EffectFilterCleanWork->AddDependency( (*SSM)->GetBufferUpdate3DWork() );
                 }
