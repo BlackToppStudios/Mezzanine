5936123773efdc6367f3c7ff957dcb1cc4acac7c
Mako_Energy <makoenergy02@gmail.com>
2013-09-20 15:54:52 -0500

    Moved over the code from the ActorSoft class to the SoftProxy, and put a NOT_IMPLEMENTED_EXCEPTION in it's constructor.

--- Mezzanine/src/Physics/softproxy.cpp
+++ Mezzanine/src/Physics/softproxy.cpp
@@ -41,21 +41,241 @@ John Blackwood - makoenergy02@gmail.com
 #define _softproxy_cpp
 
 #include "Physics/softproxy.h"
+#include "Physics/physicsmanager.h"
+
+#include "enumerations.h"
+#include "stringtool.h"
+
+//#include "Internal/meshtools.h.cpp"
+
+#include <btBulletDynamicsCommon.h>
+#include <BulletSoftBody/btSoftRigidDynamicsWorld.h>
 
 namespace Mezzanine
 {
     namespace Physics
     {
-        SoftProxy::SoftProxy(PhysicsManager* Creator) :
-            PhysicsProxy(Creator)
+        /*void _Update()
         {
+            //Ogre::VertexData* vertexData = entity->getMesh()->getSubMesh(0)->vertexData;
 
+            //const Ogre::VertexElement* posElem = vertexData->vertexDeclaration->findElementBySemantic(Ogre::VES_POSITION);
+            //Ogre::HardwareVertexBufferSharedPtr vBuffer = vertexData->vertexBufferBinding->getBuffer(posElem->getSource());
+
+            //unsigned char* vertex = static_cast<unsigned char*>(vBuffer->lock(Ogre::HardwareBuffer::HBL_NORMAL));
+            //float* pReal = NULL;
+            //for (size_t j = 0; j < vertexData->vertexCount; j++ )
+            //{
+            //    posElem->baseVertexPointerToElement(vertex, &pReal);
+            //    *pReal++ = this->PhysicsSoftBody->m_nodes[j].m_x.x();
+            //    *pReal++ = this->PhysicsSoftBody->m_nodes[j].m_x.y();
+            //    *pReal++ = this->PhysicsSoftBody->m_nodes[j].m_x.z();
+            //    vertex += vBuffer->getVertexSize();
+            //}
+            //vBuffer->unlock();
+
+            //btVector3 position = this->PhysicsSoftBody->getWorldTransform().getOrigin();
+            //btQuaternion rotation = this->PhysicsSoftBody->getWorldTransform().getRotation();
+            btVector3 position = this->PhysicsSoftBody->m_clusters[0]->m_framexform.getOrigin();
+            btQuaternion rotation = this->PhysicsSoftBody->m_clusters[0]->m_framexform.getRotation();
+            this->GraphicsNode->setPosition(position.x(), position.y(), position.z());
+            this->GraphicsNode->setOrientation(rotation.w(), rotation.x(), rotation.y(), rotation.z());
+        }//*/
+
+
+        SoftProxy::SoftProxy(const Real Mass, PhysicsManager* Creator) :
+            PhysicsProxy(Creator),
+            PhysicsSoftBody(NULL)
+        {
+            MEZZ_EXCEPTION(Exception::NOT_IMPLEMENTED_EXCEPTION,"Soft body support is still under construction and not complete.");
+            this->CreateSoftObject(Mass);
         }
 
         SoftProxy::~SoftProxy()
         {
+            if( this->IsInWorld() )
+                this->RemoveFromWorld();
+
+            delete this->PhysicsSoftBody;
+        }
+
+        void SoftProxy::CreateSoftObject(const Real Mass)
+        {
+            /*Internal::MeshInfo CurMesh;
+            Internal::MeshTools::GetMeshVerticies(GraphicsObject,CurMesh);
+            Internal::MeshTools::GetMeshIndicies(GraphicsObject,CurMesh);
+            Internal::MeshTools::GetMeshNormals(GraphicsObject,CurMesh);
+            Internal::MeshTools::GetMeshTextures(GraphicsObject,CurMesh);
+            Internal::MeshTools::GetOtherMeshInfo(GraphicsObject,CurMesh);
+
+            this->PhysicsSoftBody = btSoftBodyHelpers::CreateFromTriMesh(Entresol::GetSingletonPtr()->GetPhysicsManager()->_GetPhysicsWorldPointer()->getWorldInfo(), &CurMesh.Verticies[0].x, &CurMesh.Indicies[0], CurMesh.ICount/3);
+            PhysicsObject=PhysicsSoftBody;
+            PhysicsObject->setUserPointer( (WorldObject*)this );
+            PhysicsShape = PhysicsSoftBody->getCollisionShape();
+            PhysicsSoftBody->setTotalMass(mass, true);
+            PhysicsSoftBody->m_cfg.collisions = btSoftBody::fCollision::CL_SS + btSoftBody::fCollision::CL_RS;
+            PhysicsSoftBody->m_cfg.piterations = 5;
+            PhysicsSoftBody->randomizeConstraints();
+            PhysicsSoftBody->generateBendingConstraints(2);
+            PhysicsSoftBody->generateClusters(20);
+
+            CreateManualMesh(CurMesh);
+
+            this->GraphicsObject = Entresol::GetSingletonPtr()->GetSceneManager()->GetGraphicsWorldPointer()->createEntity(CurMesh.Name, CurMesh.Name + "M", CurMesh.Group);
+            Ogre::Any OgreRef( (WorldObject*)this );
+            GraphicsObject->setUserAny(OgreRef);
+
+            this->PhysicsSoftBody->m_clusters[0]->m_collide = true;//*/
+
+            // CreateManualMesh
+            /*Ogre::ManualObject* ManualEntity = new Ogre::ManualObject(TheMesh.Name);
+            ManualEntity->setDynamic(true);
+            ManualEntity->estimateVertexCount(TheMesh.VCount);
+            ManualEntity->estimateIndexCount(TheMesh.ICount);
+
+            ManualEntity->begin(TheMesh.Material, TheMesh.RendOp, TheMesh.Group);
+            for( int x=0 ; x < TheMesh.VCount ; x++ )
+            {
+                ManualEntity->position(TheMesh.Verticies[x]);
+                ManualEntity->normal(TheMesh.Normals[x]);
+                ManualEntity->textureCoord(TheMesh.Textures[x]);
+            }
+            for( int y=0 ; y < TheMesh.ICount ; y++ )
+            {
+                ManualEntity->index(TheMesh.Indicies[y]);
+            }
+            ManualEntity->end();
+            ManualEntity->convertToMesh(TheMesh.Name + "M", TheMesh.Group);
+            delete ManualEntity;//*/
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////
+        // Utility
+
+        Mezzanine::ProxyType SoftProxy::GetProxyType() const
+        {
+            return Mezzanine::PT_Physics_SoftProxy;
+        }
+
+        void SoftProxy::AddToWorld()
+        {
+            if( !this->IsInWorld() ) {
+                this->Manager->_GetPhysicsWorldPointer()->addSoftBody( this->PhysicsSoftBody, this->CollisionGroup, this->CollisionMask );
+            }
+        }
+
+        void SoftProxy::RemoveFromWorld()
+        {
+            if( this->IsInWorld() ) {
+                this->Manager->_GetPhysicsWorldPointer()->removeSoftBody( this->PhysicsSoftBody );
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////
+        // Collision Settings
+
+        void SoftProxy::SetCollisionShape(CollisionShape* Shape)
+        {
+            // Do nothing.  Soft bodies have their own shapes specific to the nodes and positions of the individual soft body.
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////
+        // Transform Methods
+
+        void SoftProxy::SetLocation(const Vector3& Loc)
+        {
+            //this->PhysicsSoftBody->m_clusters[0]->m_framexform.setOrigin(Location.GetBulletVector3());
+        }
+
+        void SoftProxy::SetLocation(const Real X, const Real Y, const Real Z)
+        {
 
         }
+
+        Vector3 SoftProxy::GetLocation() const
+        {
+            return Vector3(0,0,0);
+            //Vector3 temp(this->PhysicsSoftBody->m_clusters[0]->m_framexform.getOrigin());
+            //return temp;
+        }
+
+        void SoftProxy::SetOrientation(const Quaternion& Ori)
+        {
+            //this->PhysicsSoftBody->m_clusters[0]->m_framexform.setRotation(Rotation.GetBulletQuaternion(true));
+        }
+
+        void SoftProxy::SetOrientation(const Real X, const Real Y, const Real Z, const Real W)
+        {
+
+        }
+
+        Quaternion SoftProxy::GetOrientation() const
+        {
+            return Quaternion(0,0,0,1);
+        }
+
+        void SoftProxy::SetScale(const Vector3& Sc)
+        {
+
+        }
+
+        void SoftProxy::SetScale(const Real X, const Real Y, const Real Z)
+        {
+
+        }
+
+        Vector3 SoftProxy::GetScale() const
+        {
+            return Vector3(1,1,1);
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////
+        // Serialization
+
+        void SoftProxy::ProtoSerialize(XML::Node& ParentNode) const
+        {
+
+        }
+
+        void SoftProxy::ProtoSerializeProperties(XML::Node& SelfRoot) const
+        {
+
+        }
+
+        void SoftProxy::ProtoSeriailzeNodes(XML::Node& SelfRoot) const
+        {
+
+        }
+
+        void SoftProxy::ProtoDeSerialize(const XML::Node& SelfRoot)
+        {
+
+        }
+
+        void SoftProxy::ProtoDeSerializeProperties(const XML::Node& SelfRoot)
+        {
+
+        }
+
+        void SoftProxy::ProtoDeSeriailzeNodes(XML::Node& SelfRoot) const
+        {
+
+        }
+
+        String SoftProxy::GetDerivedSerializableName() const
+            { return SoftProxy::GetSerializableName(); }
+
+        String SoftProxy::GetSerializableName()
+            { return "SoftProxy"; }
+
+        ///////////////////////////////////////////////////////////////////////////////
+        // Internal Methods
+
+        btSoftBody* SoftProxy::_GetPhysicsObject() const
+            { return this->PhysicsSoftBody; }
+
+        btCollisionObject* SoftProxy::_GetBasePhysicsObject() const
+            { return this->PhysicsSoftBody; }
     }//Physics
 }//Mezzanine
 
--- Mezzanine/src/Physics/softproxy.h
+++ Mezzanine/src/Physics/softproxy.h
@@ -42,24 +42,108 @@ John Blackwood - makoenergy02@gmail.com
 
 #include "Physics/physicsproxy.h"
 
+class btSoftBody;
+
 namespace Mezzanine
 {
     namespace Physics
     {
         ///////////////////////////////////////////////////////////////////////////////
-		/// @brief This is the proxy object for soft/compressable bodies.
-		/// @details
-		///////////////////////////////////////
-		class MEZZ_LIB SoftProxy : public PhysicsProxy
-		{
+        /// @brief This is the proxy object for soft/compressable bodies.
+        /// @details
+        ///////////////////////////////////////
+        class MEZZ_LIB SoftProxy : public PhysicsProxy
+        {
         protected:
+            /// @internal
+            /// @brief SoftBody proxy used by the internal physics.
+            btSoftBody* PhysicsSoftBody;
+
+            /// @internal
+            /// @brief Used to create the physics representation of the soft body.
+            /// @param Mass The mass of the soft body to be created.
+            virtual void CreateSoftObject(const Real Mass);
         public:
             /// @brief Class constructor.
+            /// @param Mass The combined mass of the nodes in the soft body.
             /// @param Creator A pointer to the manager that created this proxy.
-            SoftProxy(PhysicsManager* Creator);
+            SoftProxy(const Real Mass, PhysicsManager* Creator);
             /// @brief Class destructor.
             virtual ~SoftProxy();
-		};//SoftProxy
+
+            ///////////////////////////////////////////////////////////////////////////////
+            // Utility
+
+            /// @copydoc WorldProxy::GetProxyType() const
+            virtual Mezzanine::ProxyType GetProxyType() const;
+
+            /// @copydoc WorldProxy::AddToWorld()
+            virtual void AddToWorld();
+            /// @copydoc WorldProxy::RemoveFromWorld()
+            virtual void RemoveFromWorld();
+
+            ///////////////////////////////////////////////////////////////////////////////
+            // Collision Settings
+
+            /// @copydoc PhysicsProxy::SetCollisionShape(CollisionShape*)
+            virtual void SetCollisionShape(CollisionShape* Shape);
+
+            ///////////////////////////////////////////////////////////////////////////////
+            // Transform Methods
+
+            /// @copydoc WorldProxy::SetLocation(const Vector3&)
+            virtual void SetLocation(const Vector3& Loc);
+            /// @copydoc WorldProxy::SetLocation(const Real, const Real, const Real)
+            virtual void SetLocation(const Real X, const Real Y, const Real Z);
+            /// @copydoc WorldProxy::GetLocation() const
+            virtual Vector3 GetLocation() const;
+            /// @copydoc WorldProxy::SetOrientation(const Quaternion&)
+            virtual void SetOrientation(const Quaternion& Ori);
+            /// @copydoc WorldProxy::SetOrientation(const Real, const Real, const Real, const Real)
+            virtual void SetOrientation(const Real X, const Real Y, const Real Z, const Real W);
+            /// @copydoc WorldProxy::GetOrientation() const
+            virtual Quaternion GetOrientation() const;
+            /// @copydoc PhysicsProxy::SetScale(const Vector3&)
+            virtual void SetScale(const Vector3& Sc);
+            /// @copydoc PhysicsProxy::SetScale(const Real, const Real, const Real)
+            virtual void SetScale(const Real X, const Real Y, const Real Z);
+            /// @copydoc WorldProxy::GetScale() const
+            virtual Vector3 GetScale() const;
+
+            ///////////////////////////////////////////////////////////////////////////////
+            // Serialization
+
+            /// @copydoc WorldProxy::ProtoSerialize(XML::Node&) const
+            virtual void ProtoSerialize(XML::Node& ParentNode) const;
+            /// @copydoc PhysicsProxy::ProtoSerializeProperties(XML::Node&) const
+            virtual void ProtoSerializeProperties(XML::Node& SelfRoot) const;
+            /// @brief Convert the nodes (and their specific properties) of this class to an XML::Node ready for serialization.
+            /// @param SelfRoot The root node containing all the serialized data for this instance.
+            virtual void ProtoSeriailzeNodes(XML::Node& SelfRoot) const;
+
+            /// @copydoc WorldProxy::ProtoDeSerialize(const XML::Node&)
+            virtual void ProtoDeSerialize(const XML::Node& SelfRoot);
+            /// @copydoc PhysicsProxy::ProtoDeSerializeProperties(const XML::Node&)
+            virtual void ProtoDeSerializeProperties(const XML::Node& SelfRoot);
+            /// @brief Take the data stored in an XML Node and overwrite the nodes (and their specific properties) of this object with it.
+            /// @param SelfRoot An XML::Node containing the data to populate this class with.
+            virtual void ProtoDeSeriailzeNodes(XML::Node& SelfRoot) const;
+
+            /// @copydoc WorldProxy::GetDerivedSerializableName() const
+            virtual String GetDerivedSerializableName() const;
+            /// @copydoc WorldProxy::GetSerializableName()
+            static String GetSerializableName();
+
+            ///////////////////////////////////////////////////////////////////////////////
+            // Internal Methods
+
+            /// @internal
+            /// @brief Accessor for the internal soft body physics proxy.
+            /// @return Returns a pointer to the internal proxy this proxy is based on.
+            virtual btSoftBody* _GetPhysicsObject() const;
+            /// @copydoc PhysicsProxy::_GetBasePhysicsObject()
+            virtual btCollisionObject* _GetBasePhysicsObject() const;
+        };//SoftProxy
     }//Physics
 }//Mezzanine
 
