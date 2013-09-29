97d24a6518400427f85b6dc98db93fc6da72e37d
Mako_Energy <makoenergy02@gmail.com>
2013-09-29 18:18:43 -0500

    Fixes to include guards, so they describe the namespace they are in.

--- Mezzanine/src/Graphics/scenemanager.h
+++ Mezzanine/src/Graphics/scenemanager.h
@@ -37,8 +37,8 @@
    Joseph Toppi - toppij@gmail.com
    John Blackwood - makoenergy02@gmail.com
 */
-#ifndef _scenemanager_h
-#define _scenemanager_h
+#ifndef _graphicsscenemanager_h
+#define _graphicsscenemanager_h
 
 #include "colourvalue.h"
 #include "worldmanager.h"
