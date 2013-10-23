92faceea02494fa00d65d2566e6810609c709110
Mako_Energy <makoenergy02@gmail.com>
2013-10-22 21:24:32 -0500

    Implemented proxy serialization for AreaEffects.

--- Mezzanine/src/areaeffect.cpp
+++ Mezzanine/src/areaeffect.cpp
@@ -272,7 +272,18 @@ namespace Mezzanine
 
     void AreaEffect::ProtoSerializeProxies(XML::Node& SelfRoot) const
     {
+        // No base implementations to call
+        XML::Node ProxiesNode = SelfRoot.AppendChild( AreaEffect::GetSerializableName() + "Proxies" );
 
+        if( ProxiesNode.AppendAttribute("Version").SetValue("1") )
+        {
+            XML::Node GhostProxNode = ProxiesNode.AppendChild("Ghost");
+            this->Ghost->ProtoSerialize( GhostProxNode );
+
+            return;
+        }else{
+            SerializeError("Create XML Attribute Values",AreaEffect::GetSerializableName() + "Proxies",true);
+        }
     }
 
     void AreaEffect::ProtoDeSerializeProperties(const XML::Node& SelfRoot)
@@ -282,7 +293,31 @@ namespace Mezzanine
 
     void AreaEffect::ProtoDeSerializeProxies(const XML::Node& SelfRoot)
     {
+        this->DestroyAreaEffect();
+        // No base implementations to call
+        //XML::Attribute CurrAttrib;
+        XML::Node ProxiesNode = SelfRoot.GetChild( AreaEffect::GetSerializableName() + "Proxies" );
 
+        if( !ProxiesNode.Empty() ) {
+            if(ProxiesNode.GetAttribute("Version").AsInt() == 1) {
+                /// @todo I don't think an exception is appropriate for the failure of the worldmanager validity checks,
+                /// however a warning should be written to the log if that happens.  This should be updated to do that once
+                /// logging refactors are done.
+
+                XML::Node GhostProxNode = ProxiesNode.GetChild("Ghost").GetFirstChild();
+                if( !GhostProxNode.Empty() ) {
+                    Physics::PhysicsManager* PhysMan = Entresol::GetSingletonPtr()->GetPhysicsManager();
+                    if( PhysMan ) {
+                        this->Ghost = PhysMan->CreateGhostProxy(GhostProxNode);
+                        this->Ghost->_Bind( this );
+                    }
+                }
+            }else{
+                MEZZ_EXCEPTION(Exception::INVALID_VERSION_EXCEPTION,"Incompatible XML Version for " + (AreaEffect::GetSerializableName() + "Proxies" ) + ": Not Version 1.");
+            }
+        }else{
+            MEZZ_EXCEPTION(Exception::II_IDENTITY_NOT_FOUND_EXCEPTION,AreaEffect::GetSerializableName() + "Proxies" + " was not found in the provided XML node, which was expected.");
+        }
     }
 
     String AreaEffect::GetDerivedSerializableName() const
@@ -340,11 +375,7 @@ namespace Mezzanine
             return;
 
         if( this->Ghost == ToBeDestroyed ) {
-            Physics::PhysicsManager* PhysMan = Entresol::GetSingletonPtr()->GetPhysicsManager();
-            if( PhysMan != NULL ) {
-                PhysMan->DestroyProxy( this->Ghost );
-                this->Ghost = NULL;
-            }
+            this->Ghost = NULL;
         }
     }
 }
