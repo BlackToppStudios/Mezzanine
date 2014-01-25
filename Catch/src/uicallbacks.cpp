c5a5f48395b44c7179d21209aa9e3b729340f588
Mako_Energy <makoenergy02@gmail.com>
2014-01-24 19:12:54 -0600

    Created a couple of currently blank files that will house the new UI callbacks for Catch.

---
+++ Catch/src/uicallbacks.cpp
@@ -0,0 +1,8 @@
+#ifndef _uicallbacks_cpp
+#define _uicallbacks_cpp
+
+#include "uicallbacks.h"
+
+
+
+#endif
---
+++ Catch/src/uicallbacks.h
@@ -0,0 +1,10 @@
+#ifndef _uicallbacks_h
+#define _uicallbacks_h
+
+#include <mezzanine.h>
+
+using namespace Mezzanine;
+
+
+
+#endif
