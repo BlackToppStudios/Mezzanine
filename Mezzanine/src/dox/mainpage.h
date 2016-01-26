4df45b307f1c7c8ca7e523c01e45fdd409971c50
Sqeaky <toppij@blacktoppstudios.com>
2016-01-26 00:19:38 -0600

    Added Team Description Page

--- Mezzanine/src/dox/mainpage.h
+++ Mezzanine/src/dox/mainpage.h
@@ -51,10 +51,55 @@
     @n Here are some useful link:
     - @ref tech_doc_index
     - @ref blog
+    - @ref team_and_credits
 */
 
 // The pages below are intended to be put into their own files as they are worked on.
 // All of these have links pointing to them.
+
+/**
+    @page team_and_credits Team and Credits
+
+    @section core_team Core Team
+    - Joseph Toppi – Lead Developer
+    Co-founder of BlackTopp Studios. Joseph has 17+ years of programming experience on an array of
+    languages and platforms. Nowadays he’s sharpening his skills with C++ to develop the %Mezzanine
+    Engine. Joe makes technical decisions about internal design, prefers maintanence free code
+    backed by good unit tests and likes walks on the beach (in Nebraska).
+
+    - John Blackwood – Developer and Designer
+    Co-founder of BlackTopp Studios. John started C++ development with this project back when it was
+    started in early 2010. Since then he’s learned the language at a rapid pace and is currently
+    gaining a mastery of the Mezzanine Engine’s dependency libraries and makes key decision about
+    how to best interface with and use them. John likes his code the have a sound theorhetical
+    basis, provide understandable low cost abstractions and is champion paintball camper.
+
+    - Jason Halder – Business Commando
+    CFO of BlackTopp Studios. Jason started with us in early summer of 2010 once the founders
+    realized legal stuff was extremely boring. He often attends meetings and gatherings for startup
+    and IT businesses to network and get the BTS name out there. He also brings an “everyday gamer”
+    perspective to BTS development that can often be forgotten by a designer.
+
+
+    @section catch_contributors Catch Contributors
+    - Nastasja Soto – 3D and 2D Artist
+    Cool game artist with an impossible-to-say name from Chile who enjoys making any game related
+    stuff. When BlackTopp studios was needing a Blender artist, they got in contact with her and
+    she was in charge of making some format conversions using Blender, then she started making
+    some art concepts for Catch!. Since then they work together and live happily ever after.
+
+    - Gary Smith – 3D and 2D Artist
+    The first artist discovered by BlackTopp Studios. Gary is a game artist with a not-so-impossible
+    to say name from Virginia. Gary was first discovered by a chancy post on Deviant art and he
+    stepped up to the challenge of game art.  He is a skilled and very detail-oriented artist,
+    fluent with 3D Studio Max, Gary helps with the creation of 3D art for Catch!.
+
+    @section mezzanine_contributors Mezzanine Contributors
+    - Kirk Miller
+    - Mike Romeu
+
+
+*/
 
 
 /**
