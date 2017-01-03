f74b20efa0e3df62df731ea2e6f7ae734781bd71
MakoEnergy <blackwoodj@blacktoppstudios.com>
2017-01-03 03:04:24 -0600

    Minor doc fix.

--- Mezzanine/src/Audio/effect.h
+++ Mezzanine/src/Audio/effect.h
@@ -1,4 +1,4 @@
-// Â© Copyright 2010 - 2016 BlackTopp Studios Inc.
+// © Copyright 2010 - 2016 BlackTopp Studios Inc.
 /* This file is part of The Mezzanine Engine.
 
     The Mezzanine Engine is free software: you can redistribute it and/or modify
@@ -56,7 +56,6 @@ namespace Mezzanine
     {
         ///////////////////////////////////////////////////////////////////////////////
         /// @brief This is an interface class for an effect that can be applied to a sound.
-        /// @details
         ///////////////////////////////////////
         class iEffect
         {
